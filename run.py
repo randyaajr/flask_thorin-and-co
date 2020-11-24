0bd9fae87b2b68d94489564f9abf94939abae02b
randyaajr <randyaajrsp@gmail.com>
2020-11-24 00:24:54 +0000

    Completed the forms page and fuctions for said page to work

--- run.py
+++ run.py
@@ -1,14 +1,17 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
 def index():
-    return render_template("index.html",)
+    return render_template("index.html")
 
 
 @app.route("/about")
@@ -16,7 +19,7 @@ def about():
     data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("about.html", page_title="About", company = data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/about/<member_name>")
@@ -27,11 +30,14 @@ def about_member(member_name):
         for obj in data:
             if obj["url"] == member_name:
                 member = obj
-    return render_template("member.html", member = member)
+    return render_template("member.html", member=member)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
